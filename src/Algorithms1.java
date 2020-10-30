addc040d77dc333af9b2c9409334759f5f62410c
Danijel Gitanovic <dani025a@edu.ucl.dk>
2020-10-30 11:47:00 +0100

    Har lavet test for drenge og pige navne, med seeds

--- src/Algorithms1.java
+++ src/Algorithms1.java
@@ -8,46 +8,31 @@ import java.util.Random;
  */
 
 public class Algorithms1 {
-
+    Random rand = new Random();
     // TODO 1 - Gennemse Data klassen, og skriv gode kommentarer til det
 
     // TODO 2 - Skriv en randomBoyName() metode i Algorithms1, som returnerer et tilfældigt drengenavn fra Data klassens randomDrengeNavne property OBS: randomDrengeNavne skal forblive private!
 
-    public static String randomBoyName() {
-        Random rand = new Random();
-
-        String[] names = new Data().getRandomDrengeNavne();
-
+    public String randomBoyName() {
+        String[] names;
+        names = new Data().getRandomDrengeNavne();
         return names[rand.nextInt(names.length)];
     }
+
     // TODO 3 - Skriv en test til randomBoyName() metoden HINT: Se metoden exampleOfPredictableRandomNumber() for at se, hvordan du kan lave et tilfældigt nummer som er altid det samme (til test)
 
     // TODO 4 - Skriv en randomGirlName() metode i Algorithms1, som returnerer et tilfældigt pigenavn fra Data klassen
 
-    public static String randomGirlName(){
-        Random rand = new Random();
-
-        String[] names = new Data().getRandomPigeNavne();
-
+    public String randomGirlName() {
+        String[] names;
+        names = new Data().getRandomPigeNavne();
         return names[rand.nextInt(names.length)];
-
     }
 
     // TODO 5 - Skriv en randomName() metode i Algorithms1, som laver et sammenlagt array som indeholder både piger og drenge, og returnerer det
-    public static String randomName(){
-        Random rand = new Random();
 
-        String[] names =  new Data().getRandomDrengeNavne();
-
-        return names[rand.nextInt(names.length)];
-    }
 
     // TODO 6 - Skriv en test til randomName() metoden
-    private static void exampleOfName() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(randomGirlName());
-        }
-    }
 
     // TODO 7 - Skriv en random bogstavs-generator metode (du kan tage udgangspunkt i randomNumbers() i Data)
 
@@ -55,15 +40,8 @@ public class Algorithms1 {
 
     public static void main(String[] args) {
         exampleOfPredictableRandomNumber();
-        exampleOfRandomBoyNames();
 
     }
-    private static void exampleOfRandomBoyNames() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(randomBoyName());
-        }
-    }
-
     private static void exampleOfPredictableRandomNumber() {
         Random random = new Random();
         random.setSeed(15L); // Hvis vi angiver et seed som et long number, vil random klassen altid generere det samme tal, i det her tilfælde 21
--- src/Algorithms1Test.java
+++ src/Algorithms1Test.java
@@ -1,2 +1,19 @@
-public class Algorithms1Test {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Algorithms1Test {
+
+    @Test
+    void randomBoyName() {
+        Algorithms1 a = new Algorithms1();
+        a.rand.setSeed(5);
+        assertEquals("Klausbjerke", a.randomBoyName());
+    }
+    @Test
+    void randomGirlName() {
+        Algorithms1 a = new Algorithms1();
+        a.rand.setSeed(5);
+        assertEquals("Gin", a.randomGirlName());
+    }
 }
